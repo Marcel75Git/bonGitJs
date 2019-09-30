fb81dd920364390e0ba2d66dff7af7c1d578a18d
Mfoulou Yves <myvesmarcelo@yahoo.fr>
2019-09-30 17:28:57 +0200

    inserer affiche

---
+++ App.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react'
+import {Text, StyleSheet,View} from 'react-native'
+
+ class App extends Component{
+	render(){
+		return(
+          <View style = {styles.container}>
+
+             <Text}> je teste mes valeurs </Text>
+          
+          </View>
+			)
+	}
+}
+ 
+ const styles = StyleSheet.create({
+ 	container:{
+ 		flex:1,
+ 		backgtoundColor:"green",
+ 	},
+ })
+
+
+export default App
